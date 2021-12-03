a0a48111c92daaf46a5793ba7cb683b14dfe62f6
fadhelrahmawan <57004251+fadhelrahmawan@users.noreply.github.com>
2021-12-03 23:39:41 +0700

    P4_2_1184064

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-namespace P4_1_1184064
+﻿namespace P4_2_1184064
 {
     partial class Form1
     {
@@ -28,53 +28,30 @@ namespace P4_1_1184064
         /// </summary>
         private void InitializeComponent()
         {
-            this.vbButton = new System.Windows.Forms.Button();
-            this.csButton = new System.Windows.Forms.Button();
-            this.closeButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // vbButton
+            // openFileDialog1
             // 
-            this.vbButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.vbButton.ForeColor = System.Drawing.Color.DarkGreen;
-            this.vbButton.Location = new System.Drawing.Point(186, 87);
-            this.vbButton.Name = "vbButton";
-            this.vbButton.Size = new System.Drawing.Size(121, 23);
-            this.vbButton.TabIndex = 0;
-            this.vbButton.Text = "Visual Basic";
-            this.vbButton.UseVisualStyleBackColor = true;
-            this.vbButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.vbButton_MouseMove);
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // csButton
+            // openFileButton
             // 
-            this.csButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.csButton.ForeColor = System.Drawing.Color.Red;
-            this.csButton.Location = new System.Drawing.Point(417, 87);
-            this.csButton.Name = "csButton";
-            this.csButton.Size = new System.Drawing.Size(75, 23);
-            this.csButton.TabIndex = 1;
-            this.csButton.Text = "C#";
-            this.csButton.UseVisualStyleBackColor = true;
-            this.csButton.Click += new System.EventHandler(this.csButton_Click);
-            // 
-            // closeButton
-            // 
-            this.closeButton.Location = new System.Drawing.Point(186, 251);
-            this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(75, 23);
-            this.closeButton.TabIndex = 2;
-            this.closeButton.Text = "Close";
-            this.closeButton.UseVisualStyleBackColor = true;
-            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            this.openFileButton.Location = new System.Drawing.Point(151, 100);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(75, 23);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "saya";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.closeButton);
-            this.Controls.Add(this.csButton);
-            this.Controls.Add(this.vbButton);
+            this.Controls.Add(this.openFileButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -83,7 +60,8 @@ namespace P4_1_1184064
 
         #endregion
 
-        private System.Windows.Forms.Button vbButton;
-        private System.Windows.Forms.Button csButton;
-        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openFileButton;
     }
+}
+
