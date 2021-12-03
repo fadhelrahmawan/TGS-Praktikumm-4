cb4f9adae73f0758b5d2bca4b4f386e2942e1fdc
fadhelrahmawan <57004251+fadhelrahmawan@users.noreply.github.com>
2021-12-03 23:38:45 +0700

    P4_2_1184064

--- Form1.cs
+++ Form1.cs
@@ -1,22 +1,51 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace P4_1_1184064
+namespace P4_2_1184064
 {
-    internal static class Program
+    public partial class Form1 : Form
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        private string filePath;
+        public Form1()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult userResponse = openFileDialog1.ShowDialog();
+                if (userResponse == DialogResult.OK)
+                {
+                    filePath = openFileDialog1.FileName.ToString();
+                    MessageBox.Show("You Successfully opened: " + filePath + "'", "Success",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information,
+                        MessageBoxDefaultButton.Button1);
+                }
+                else
+                    MessageBox.Show("You canceled the open file operation", "Warning",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning,
+                        MessageBoxDefaultButton.Button1,
+                        MessageBoxOptions.RightAlign);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
         }
     }
 }
