2d539feaa467faa7c9de2d53c83feda6db5248dc
fadhelrahmawan <57004251+fadhelrahmawan@users.noreply.github.com>
2021-12-03 23:31:18 +0700

    P4_1_1184064

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+namespace P4_1_1184064
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vbButton = new System.Windows.Forms.Button();
+            this.csButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // vbButton
+            // 
+            this.vbButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vbButton.ForeColor = System.Drawing.Color.DarkGreen;
+            this.vbButton.Location = new System.Drawing.Point(186, 87);
+            this.vbButton.Name = "vbButton";
+            this.vbButton.Size = new System.Drawing.Size(121, 23);
+            this.vbButton.TabIndex = 0;
+            this.vbButton.Text = "Visual Basic";
+            this.vbButton.UseVisualStyleBackColor = true;
+            this.vbButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.vbButton_MouseMove);
+            // 
+            // csButton
+            // 
+            this.csButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.csButton.ForeColor = System.Drawing.Color.Red;
+            this.csButton.Location = new System.Drawing.Point(417, 87);
+            this.csButton.Name = "csButton";
+            this.csButton.Size = new System.Drawing.Size(75, 23);
+            this.csButton.TabIndex = 1;
+            this.csButton.Text = "C#";
+            this.csButton.UseVisualStyleBackColor = true;
+            this.csButton.Click += new System.EventHandler(this.csButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(186, 251);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.csButton);
+            this.Controls.Add(this.vbButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button vbButton;
+        private System.Windows.Forms.Button csButton;
+        private System.Windows.Forms.Button closeButton;
+    }
